3c10f133c2af1a9d6e4e2f293b8fd391414bbca3
Mrdeniganz <46273749+Mrdeniganz@users.noreply.github.com>
2019-03-06 21:00:46 +0700

    Add files via upload

---
+++ bank.sh
@@ -0,0 +1,84 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+echo ""
+echo $red"L"
+sleep 2
+clear
+echo $green"LO"
+sleep 2
+clear
+echo $yellow"LOA"
+sleep 2
+clear
+echo $cyan"LOAD"
+sleep 2
+clear
+echo $purple"LOADI"
+sleep 2
+clear
+echo $green"LOADING...."
+sleep 2
+clear
+echo
+echo
+toilet -f standard "HACK BANK" -F gay
+sleep 2
+echo 
+echo $green"Created by InYour • 3M0N27"
+echo $green"type : nuyul bank"
+echo $green"Tolong gunakan dengan bijak"
+echo $yellow"============================"
+read -p "Masukan nama bank(lokasi) : " lokasi
+
+echo $green"Sendang mencari lokasi......."
+sleep 3
+echo $green"Sedang mencari bank(lokasi)....."
+sleep 2
+echo $green"Menemukan lokasi.."
+sleep 2
+echo $green"Mencari system keuangan..."
+sleep 2
+read -p "Mmasukan nomor rekeningmu : " norek
+echo $green"Sedang Mengambil uang...."
+sleep 3
+echo $green"Send money to your credit card......"
+sleep 2
+echo $green"send 50.000...."
+sleep 2
+echo $green"send 50.000..."
+sleep 2
+echo $green"Send money succses.."
+sleep 2
+read -p "Apa kamu ingin mengecek saldomu?(y/n) : " saldo
+
+if [ $saldo = y ] || [ $saldo = y ]
+then
+read -p "Masukan nomor rekeningmu : " norek
+
+echo $green"Sedang mengecek saldo..."
+sleep 3
+echo $green"Saldomu saat ini Rp5.000.000"
+sleep 3
+echo $yellow"Thanks telah menggunakan Tools ini.."
+sleep 2
+echo $green"good bye :)"
+fi
+
+if [ $saldo = n ] || [ $saldo = n ]
+then
+echo $green"yakin gak mau cek saldo :) "
+sleep 2
+echo $green"ada bonus loh"
+sleep 2
+echo $yellow"iya udah lah terserah"
+sleep 2
+echo $green"good bye"
+fi
+clear
